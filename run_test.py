0943f2854424bbe128b8f4e98013064956e30934
arunrout-at-514304094524 <arun.rout@wizklub.com>
2024-07-25 17:44:50 +0530

    Docker Integration with Jenkins

--- run_test.py
+++ run_test.py
@@ -5,6 +5,7 @@ def run_behave_tests():
     try:
         print("Arun")
         print("Kumar")
+        print("Rout")
         # Activate the virtual environment
         venv_python = os.path.join('venv', 'Scripts', 'python.exe')
 
