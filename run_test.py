9e96c92a783b6f6c927932b0d80153076a0e793d
arunrout-at-514304094524 <arun.rout@wizklub.com>
2024-07-25 18:17:24 +0530

    Jenkins not triggrering issue

--- run_test.py
+++ run_test.py
@@ -5,6 +5,7 @@ def run_behave_tests():
     try:
         print("Tarun")
         print("Kuamr")
+        print("Rout")
         # Activate the virtual environment
         venv_python = os.path.join('venv', 'Scripts', 'python.exe')
 
